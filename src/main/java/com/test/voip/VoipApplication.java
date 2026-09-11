5a869e8c0c2752989b048cf98935d92439697608
Aayush <aayushc951@gmail.com>
2026-09-11 15:40:30 +0530

    Initialize Maven project structure with wrapper, configuration files, and basic application setup

---
+++ src/main/java/com/test/voip/VoipApplication.java
@@ -0,0 +1,13 @@
+package com.test.voip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/voip/VoipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.voip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
